d222930ab84dcdabc27bd08522002acbadbf3dbe
Aleksey2710 <Aleksey_2710@mail.ru>
2023-02-11 02:00:23 +0300

    initial commit

---
+++ src/main/java/pro/sky/examapp/ExamAppApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.examapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/examapp/ExamAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.examapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
